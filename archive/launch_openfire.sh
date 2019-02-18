df52c83f60fb87b38a51619fb603aa1e89491185
Fahid Mohammed <fahid.rabiu@gmail.com>
2019-02-18 23:53:02 +0000

    updating archive

---
+++ archive/launch_openfire.sh
@@ -0,0 +1,10 @@
+#For when the settings menu fails to start openfire
+
+#Navigate to openfire's directory
+cd /usr/local/openfire/bin
+
+#Set JAVA_HOME
+export JAVA_HOME='/Library/Java/JavaVirtualMachines/jdk1.8.0_121.jdk/Contents/Home'
+
+#run Openfire
+./openfire.sh
